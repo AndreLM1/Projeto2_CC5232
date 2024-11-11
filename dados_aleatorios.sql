e083bef87d50eed6d33ab9815b5db5790025ef5e
André Loureço Marques <118229460+AndreLM1@users.noreply.github.com>
2024-11-11 20:33:06 -0300

    Add files via upload

---
+++ dados_aleatorios.sql
@@ -0,0 +1,125 @@
+-- Dados para a tabela USUARIO
+INSERT INTO USUARIO (Nome, Email, Data_Registro) VALUES ('Jacob Rogers', 'mwalters@example.net', '2024-06-26');
+INSERT INTO USUARIO (Nome, Email, Data_Registro) VALUES ('Melissa Craig', 'rebeccataylor@example.net', '2024-09-12');
+INSERT INTO USUARIO (Nome, Email, Data_Registro) VALUES ('Frederick Green', 'krobertson@example.com', '2024-05-09');
+INSERT INTO USUARIO (Nome, Email, Data_Registro) VALUES ('Mary Kelly', 'lnash@example.com', '2024-05-27');
+INSERT INTO USUARIO (Nome, Email, Data_Registro) VALUES ('Anthony Burke', 'samanthaneal@example.org', '2024-01-19');
+INSERT INTO USUARIO (Nome, Email, Data_Registro) VALUES ('Lisa Baker', 'justin57@example.com', '2024-05-28');
+INSERT INTO USUARIO (Nome, Email, Data_Registro) VALUES ('Ray Freeman', 'hardingdavid@example.org', '2024-06-24');
+INSERT INTO USUARIO (Nome, Email, Data_Registro) VALUES ('Denise Goodman', 'luis02@example.org', '2024-05-06');
+INSERT INTO USUARIO (Nome, Email, Data_Registro) VALUES ('Penny Mathis', 'eayala@example.net', '2024-08-25');
+INSERT INTO USUARIO (Nome, Email, Data_Registro) VALUES ('David Patterson', 'david65@example.com', '2024-03-14');
+
+-- Dados para a tabela LISTA
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('3:43', '2024-02-25', 6);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('1:08', '2024-11-07', 7);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('2:13', '2024-01-03', 8);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('3:17', '2024-03-08', 9);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('1:40', '2024-10-01', 9);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('1:14', '2024-08-22', 9);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('3:07', '2024-07-15', 10);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('2:35', '2024-04-26', 4);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('1:35', '2024-01-08', 2);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('3:22', '2024-03-28', 2);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('3:01', '2024-07-13', 9);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('2:32', '2024-04-15', 4);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('3:16', '2024-06-15', 9);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('1:40', '2024-02-21', 10);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('2:19', '2024-06-26', 8);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('1:40', '2024-05-10', 8);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('2:03', '2024-05-09', 5);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('3:11', '2024-05-27', 3);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('3:15', '2024-02-16', 2);
+INSERT INTO LISTA (Duracao, DATA_Criacao, ID_Usuario) VALUES ('2:44', '2024-07-11', 1);
+
+-- Dados para a tabela FILME
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('General how.', 86, '2014-09-14');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('Administration list ready.', 87, '2000-11-09');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('Center behind.', 114, '2009-11-23');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('Left director.', 159, '2014-02-10');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('Beat later old several.', 88, '2021-03-03');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('Tree activity.', 177, '2006-08-07');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('From receive education.', 120, '2011-01-22');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('Free trade report.', 93, '2014-02-19');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('West reason environment group.', 132, '2011-06-15');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('To everybody force everything.', 146, '2007-02-01');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('Cold.', 157, '2009-12-30');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('Role born human.', 82, '2000-03-16');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('List white force.', 176, '2016-12-11');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('Because maybe again.', 153, '2007-05-13');
+INSERT INTO FILME (Titulo, Duracao, Data_lancamento) VALUES ('Play stuff also.', 171, '2002-02-07');
+
+-- Dados para a tabela SERIE
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Resource.', 23, '2018-05-03');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Training.', 24, '2011-11-12');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Customer agent deal.', 30, '2014-02-27');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Anyone.', 44, '2018-12-29');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Year house interview.', 46, '2012-09-20');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Almost bring even.', 44, '2013-08-03');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('President show hour.', 35, '2020-10-26');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Suggest while house.', 30, '2003-05-14');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Kid.', 54, '2000-07-06');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Head give would.', 36, '2015-02-10');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Choice information.', 45, '2021-04-12');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Major.', 35, '2016-10-18');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Former enjoy morning.', 52, '2016-08-13');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Simply benefit.', 38, '2011-09-16');
+INSERT INTO SERIE (Titulo, Duracao, Data_lancamento) VALUES ('Modern out.', 29, '2000-04-05');
+
+-- Dados para a tabela DIRETOR
+INSERT INTO DIRETOR (Nome, Data_Nascimento) VALUES ('Kathryn Sandoval', '1979-10-25');
+INSERT INTO DIRETOR (Nome, Data_Nascimento) VALUES ('Ronald Singleton', '1965-05-18');
+INSERT INTO DIRETOR (Nome, Data_Nascimento) VALUES ('Nicholas Villa', '1994-10-06');
+INSERT INTO DIRETOR (Nome, Data_Nascimento) VALUES ('Justin Franklin', '1988-02-28');
+INSERT INTO DIRETOR (Nome, Data_Nascimento) VALUES ('Crystal Rice', '1963-04-22');
+INSERT INTO DIRETOR (Nome, Data_Nascimento) VALUES ('Ashlee Lewis', '1988-01-11');
+INSERT INTO DIRETOR (Nome, Data_Nascimento) VALUES ('Danny Murphy', '1988-12-04');
+INSERT INTO DIRETOR (Nome, Data_Nascimento) VALUES ('Felicia Scott', '1972-12-14');
+INSERT INTO DIRETOR (Nome, Data_Nascimento) VALUES ('Mathew Parker', '1973-05-20');
+INSERT INTO DIRETOR (Nome, Data_Nascimento) VALUES ('Karina Green', '1962-04-30');
+
+-- Dados para a tabela LISTA_CONTEUDO
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (16, 11, NULL);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (13, NULL, 15);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (20, NULL, 7);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (10, NULL, 9);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (4, 13, NULL);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (18, 1, NULL);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (8, NULL, 4);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (10, 15, NULL);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (16, 2, NULL);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (3, NULL, 1);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (5, NULL, 4);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (15, NULL, 7);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (9, 4, NULL);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (5, NULL, 7);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (10, NULL, 10);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (13, NULL, 6);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (4, 12, NULL);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (12, 14, NULL);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (4, NULL, 3);
+INSERT INTO LISTA_CONTEUDO (ID_Lista, ID_Filme, ID_Serie) VALUES (8, 12, NULL);
+
+-- Dados para a tabela Diretor_Filme
+INSERT INTO Diretor_Filme (ID_Diretor, ID_Filme) VALUES (8, 15);
+INSERT INTO Diretor_Filme (ID_Diretor, ID_Filme) VALUES (2, 1);
+INSERT INTO Diretor_Filme (ID_Diretor, ID_Filme) VALUES (8, 8);
+INSERT INTO Diretor_Filme (ID_Diretor, ID_Filme) VALUES (6, 11);
+INSERT INTO Diretor_Filme (ID_Diretor, ID_Filme) VALUES (4, 8);
+INSERT INTO Diretor_Filme (ID_Diretor, ID_Filme) VALUES (1, 6);
+INSERT INTO Diretor_Filme (ID_Diretor, ID_Filme) VALUES (2, 12);
+INSERT INTO Diretor_Filme (ID_Diretor, ID_Filme) VALUES (9, 12);
+INSERT INTO Diretor_Filme (ID_Diretor, ID_Filme) VALUES (1, 7);
+INSERT INTO Diretor_Filme (ID_Diretor, ID_Filme) VALUES (1, 15);
+
+-- Dados para a tabela Diretor_Serie
+INSERT INTO Diretor_Serie (ID_Diretor, ID_Serie) VALUES (9, 2);
+INSERT INTO Diretor_Serie (ID_Diretor, ID_Serie) VALUES (7, 6);
+INSERT INTO Diretor_Serie (ID_Diretor, ID_Serie) VALUES (10, 9);
+INSERT INTO Diretor_Serie (ID_Diretor, ID_Serie) VALUES (5, 13);
+INSERT INTO Diretor_Serie (ID_Diretor, ID_Serie) VALUES (3, 6);
+INSERT INTO Diretor_Serie (ID_Diretor, ID_Serie) VALUES (6, 7);
+INSERT INTO Diretor_Serie (ID_Diretor, ID_Serie) VALUES (9, 10);
+INSERT INTO Diretor_Serie (ID_Diretor, ID_Serie) VALUES (9, 7);
+INSERT INTO Diretor_Serie (ID_Diretor, ID_Serie) VALUES (6, 3);
+INSERT INTO Diretor_Serie (ID_Diretor, ID_Serie) VALUES (2, 13);
