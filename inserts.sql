29ca50fc14f978395e76537f43a7f55c9a9732ee
André Loureço Marques <118229460+AndreLM1@users.noreply.github.com>
2024-11-17 13:48:03 -0300

    Add files via upload

---
+++ inserts.sql
@@ -0,0 +1,79 @@
+-- Comandos para a tabela USUARIO
+INSERT INTO USUARIO VALUES (NULL, 'Tammy Johnson', 'davidcontreras@example.com', 2022-11-28);
+INSERT INTO USUARIO VALUES (NULL, 'Charles Campbell', 'yphillips@example.org', 2022-12-18);
+INSERT INTO USUARIO VALUES (NULL, 'Daniel Harper', 'preston78@example.com', 2024-08-25);
+INSERT INTO USUARIO VALUES (NULL, 'Dr. John Morris', 'terrylisa@example.net', 2023-05-09);
+INSERT INTO USUARIO VALUES (NULL, 'Nicholas Morrison', 'kristina03@example.org', 2024-09-04);
+INSERT INTO USUARIO VALUES (NULL, 'Zachary Brown', 'tammyjimenez@example.net', 2023-07-22);
+INSERT INTO USUARIO VALUES (NULL, 'Shawn Hunt', 'hectorcowan@example.com', 2024-04-30);
+INSERT INTO USUARIO VALUES (NULL, 'Samantha Johnson', 'jamesprince@example.org', 2024-05-10);
+INSERT INTO USUARIO VALUES (NULL, 'Maria Campbell', 'smithvanessa@example.org', 2023-03-30);
+INSERT INTO USUARIO VALUES (NULL, 'Timothy Martinez', 'seanford@example.org', 2024-06-17);
+
+-- Comandos para a tabela LISTA
+INSERT INTO LISTA VALUES (NULL, 2024-02-27, 9);
+INSERT INTO LISTA VALUES (NULL, 2024-06-17, 4);
+INSERT INTO LISTA VALUES (NULL, 2023-12-22, 5);
+INSERT INTO LISTA VALUES (NULL, 2024-10-31, 7);
+INSERT INTO LISTA VALUES (NULL, 2024-10-21, 8);
+INSERT INTO LISTA VALUES (NULL, 2024-01-19, 10);
+INSERT INTO LISTA VALUES (NULL, 2024-07-12, 3);
+INSERT INTO LISTA VALUES (NULL, 2024-03-11, 7);
+INSERT INTO LISTA VALUES (NULL, 2024-06-06, 6);
+INSERT INTO LISTA VALUES (NULL, 2024-06-30, 7);
+INSERT INTO LISTA VALUES (NULL, 2024-01-16, 9);
+INSERT INTO LISTA VALUES (NULL, 2024-06-21, 1);
+INSERT INTO LISTA VALUES (NULL, 2024-10-17, 8);
+INSERT INTO LISTA VALUES (NULL, 2024-11-09, 5);
+INSERT INTO LISTA VALUES (NULL, 2024-07-21, 9);
+
+-- Comandos para a tabela FILME
+INSERT INTO FILME VALUES (NULL, 'Decentralized even-keeled throughput', 168, 1996-05-21);
+INSERT INTO FILME VALUES (NULL, 'Distributed even-keeled migration', 125, 2019-07-08);
+INSERT INTO FILME VALUES (NULL, 'User-friendly dynamic toolset', 85, 2008-04-29);
+INSERT INTO FILME VALUES (NULL, 'Quality-focused interactive hub', 155, 2007-09-10);
+INSERT INTO FILME VALUES (NULL, 'Object-based bifurcated throughput', 142, 1998-02-09);
+INSERT INTO FILME VALUES (NULL, 'Open-architected foreground application', 128, 2001-10-20);
+INSERT INTO FILME VALUES (NULL, 'Organized bi-directional capacity', 91, 2003-05-05);
+INSERT INTO FILME VALUES (NULL, 'Public-key solution-oriented pricing structure', 108, 2009-04-20);
+INSERT INTO FILME VALUES (NULL, 'Exclusive bi-directional strategy', 149, 2012-06-18);
+INSERT INTO FILME VALUES (NULL, 'Cloned zero tolerance neural-net', 149, 2016-07-01);
+INSERT INTO FILME VALUES (NULL, 'Universal real-time pricing structure', 169, 1996-06-09);
+INSERT INTO FILME VALUES (NULL, 'Fundamental scalable core', 86, 2008-05-08);
+INSERT INTO FILME VALUES (NULL, 'Switchable fault-tolerant policy', 115, 2020-04-17);
+INSERT INTO FILME VALUES (NULL, 'Intuitive bi-directional secured line', 116, 2002-04-07);
+INSERT INTO FILME VALUES (NULL, 'Cloned executive product', 119, 2007-07-13);
+INSERT INTO FILME VALUES (NULL, 'Innovative multimedia firmware', 130, 2002-07-31);
+INSERT INTO FILME VALUES (NULL, 'Streamlined methodical solution', 168, 2005-07-20);
+INSERT INTO FILME VALUES (NULL, 'Adaptive optimizing open system', 103, 2024-11-13);
+INSERT INTO FILME VALUES (NULL, 'Configurable composite data-warehouse', 81, 2022-05-22);
+INSERT INTO FILME VALUES (NULL, 'Down-sized encompassing firmware', 138, 2002-11-20);
+
+-- Comandos para a tabela SERIE
+INSERT INTO SERIE VALUES (NULL, 'Centralized exuding middleware', 5, 2012-04-24);
+INSERT INTO SERIE VALUES (NULL, 'Enhanced system-worthy initiative', 9, 2011-07-28);
+INSERT INTO SERIE VALUES (NULL, 'Customizable methodical database', 9, 2002-03-08);
+INSERT INTO SERIE VALUES (NULL, 'Optimized human-resource info-mediaries', 7, 2021-03-14);
+INSERT INTO SERIE VALUES (NULL, 'Face-to-face bi-directional function', 1, 2010-09-12);
+INSERT INTO SERIE VALUES (NULL, 'Phased optimizing Internet solution', 2, 1998-11-30);
+INSERT INTO SERIE VALUES (NULL, 'Re-engineered real-time hardware', 10, 2018-12-20);
+INSERT INTO SERIE VALUES (NULL, 'Implemented transitional application', 7, 2010-04-21);
+INSERT INTO SERIE VALUES (NULL, 'Assimilated multimedia approach', 11, 2011-04-07);
+INSERT INTO SERIE VALUES (NULL, 'Switchable 24hour moratorium', 12, 2013-03-06);
+INSERT INTO SERIE VALUES (NULL, 'Multi-channeled demand-driven open architecture', 4, 2004-10-21);
+INSERT INTO SERIE VALUES (NULL, 'Open-architected analyzing data-warehouse', 9, 2005-05-19);
+INSERT INTO SERIE VALUES (NULL, 'Expanded executive portal', 9, 2002-03-20);
+INSERT INTO SERIE VALUES (NULL, 'Future-proofed zero administration policy', 2, 2022-08-04);
+INSERT INTO SERIE VALUES (NULL, 'Monitored encompassing Graphical User Interface', 2, 2008-12-31);
+
+-- Comandos para a tabela DIRETOR
+INSERT INTO DIRETOR VALUES (NULL, 'Jessica Garcia', 1996-04-07);
+INSERT INTO DIRETOR VALUES (NULL, 'Mathew Wood', 1994-06-02);
+INSERT INTO DIRETOR VALUES (NULL, 'Kimberly King DDS', 1971-10-17);
+INSERT INTO DIRETOR VALUES (NULL, 'Bradley Watkins', 1954-08-28);
+INSERT INTO DIRETOR VALUES (NULL, 'Michaela Blackburn', 1978-11-20);
+INSERT INTO DIRETOR VALUES (NULL, 'Christopher Hancock', 1979-12-18);
+INSERT INTO DIRETOR VALUES (NULL, 'Kelly Butler DDS', 1961-08-26);
+INSERT INTO DIRETOR VALUES (NULL, 'David Orr', 1973-09-29);
+INSERT INTO DIRETOR VALUES (NULL, 'Ashley Chapman', 1977-05-09);
+INSERT INTO DIRETOR VALUES (NULL, 'Christopher Bennett', 1991-09-22);
